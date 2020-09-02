c0874ea7bc165f47559e027c1901411c1ba53c42
John <johnmsaylor@gmail.com>
2020-09-02 09:54:22 -0400

    Loan Class

---
+++ src/com/johnmsaylor/Loan.java
@@ -0,0 +1,45 @@
+package com.johnmsaylor;
+
+public class Loan extends Account {
+
+    private void setInterest(double interest) {
+        if (interest < 0 || interest > 23) throw new IllegalArgumentException("See Acceptable Rates");
+        this.interest = interest;
+    }
+
+    private double interest;
+    private int term;
+
+    public Loan(float initialDeposit, int owner, double interest, int term) {
+        super(initialDeposit, owner);
+        this.setInterest(interest);
+        this.setTerm(term);
+    }
+
+    public void showPaymentSchedule() {
+        for (int i = 0; i < term * 12; i++ ) {
+            double remainder = calculateBalance((int) this.getBalance(), i,interest,term);
+            System.out.println(remainder);
+        }
+
+    }
+
+    private double calculateBalance(int principal, int month, double interest, int term) {
+
+        interest = (interest / 100) / 12;
+        term = term * 12;
+
+        double answer = Math.pow((1 + interest), term) - Math.pow((1 + interest), month);
+        answer = principal * answer;
+        answer = answer / (Math.pow((1 + interest), term) - 1);
+
+        return Math.ceil(answer);
+
+    }
+
+
+    public void setTerm(int term) {
+        if (term < 0 || term > 30) throw new IllegalArgumentException("See Acceptable Loan Durations");
+        this.term = term;
+    }
+}
--- src/com/johnmsaylor/Main.java
+++ src/com/johnmsaylor/Main.java
@@ -6,11 +6,13 @@ public class Main {
      var checking = new Checking(100, 111);
      var savings = new Savings(100, 222);
      var investment = new Investment(100,333, 1);
+     var loan = new Loan(100_000,4444,20.1,20);
 
 //        System.out.println(checking.interest);
 //        System.out.println(savings.interest);
 //        System.out.println(investment.calculateChange(120));
 //        System.out.println(investment.calculateChange((float) 122.2));
         System.out.println(checking.getBalance());
+        loan.showPaymentSchedule();
     }
 }
